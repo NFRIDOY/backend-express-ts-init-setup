b223b60dbea4212190d24598f7e2ccd697183e81
nfridoy <nfridoy@gmail.com>
2025-05-05 11:08:34 +0600

    ✨ server initialization to use async bootstrap function

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
+import { Server } from 'http'
 import app from './app'
-const PORT = 5000
+const PORT = 5000;
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-})
+let server: Server;
+
+async function bootstrap() {
+    server = app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`)
+    })
+}
+
+bootstrap();
