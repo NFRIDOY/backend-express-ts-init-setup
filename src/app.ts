ab4c5737e0bc4ead0d5c86f6ada9f4db3a946543
nfridoy <nfridoy@gmail.com>
2025-05-05 11:59:04 +0600

    ✨ Added Persers and  Enhance Express server with JSON parsing and POST endpoint for data reception

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const app = express()
+
+// Persers
+app.use(express.json());
  
 
 app.get('/', (req, res) => {
-    res.send('Hello World From Backend Server!!')
+    res.send('Hello World From Backend Server.')
+})
+
+app.post('/', (req: Request, res: Response) => {
+    const reqObj = req.body;
+    console.log(reqObj);
+    res.send("Data Recived");
 })
 
 export default app
