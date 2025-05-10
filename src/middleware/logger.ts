4bfd3e2208f3af99ac263aececee3cc1f12b231b
NF RIDOY <nfridoy@gmail.com>
2025-05-10 12:01:11 +0600

    ✅ refactor(logger): improve logging middleware with conditional checks
    Add conditional checks to log request body, query, and params only if they exist. This prevents unnecessary logging of undefined values and improves the clarity of log messages. Also, update log message format for consistency.

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -4,8 +4,16 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
     console.log("Logger Starts____________");
     console.log(req.url, req.hostname, req.method)
     // console.log("Request", req);
-    console.log("Request Object", req.body);
-    console.log("____________Logger Ends ");
+    if (req.body) {
+        console.log("Request Body", req.body);
+    }
+    if (req.query) {
+        // console.log("Request Query", req.query);
+    }
+    if (req.params) {
+        // console.log("Request Params", req.params);
+    }
+    console.log("Logger Ends____________");
     
     next();
 }
