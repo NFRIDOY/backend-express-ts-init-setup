7f6a66b78e47e71ac6e5fd48bd1103da61d843cd
nfridoy <nfridoy@gmail.com>
2025-08-26 17:24:17 +0600

    ✨ Added(config): Added database connection and port configuration
    Refactor server.ts to utilize configuration values for the database URL and port, enhancing flexibility and maintainability. Update routes.ts to change user route paths to include a versioned API structure, improving API organization.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,8 @@ import { app } from "../app"
 import { userRoute } from "../module/common/user/user.route"
 
 const routes = () => {
-    app.use("/user", userRoute)
+    app.use("/api/user", userRoute),
+    app.use("/api/users", userRoute)
 }
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 import { Server } from 'http'
 import app from './app'
-const PORT = 5000;
+import config from './config';
+const PORT = config.port;
+const DALABASE_URL = config.database_url;
 
 let server: Server;
 async function main() {
-    await mongoose.connect(process.env.DALABASE_URL);
+    await mongoose.connect(DALABASE_URL);
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
