cad1362cef899fb0cc17a0778515f3e17fa00622
NF RIDOY <nfridoy@gmail.com>
2025-09-13 19:28:02 +0600

    ✨ feat(student): expand student validation schema with comprehensive fields
    - Updated the student validation schema to include detailed fields for name, email, phone, date of birth, address, gender, profile image, guardian, parent, local guardian, blood group, present address, permanent address, and status.
    - Enhanced validation rules to ensure data integrity and improve user feedback during student record creation and updates.

--- src/module/student/validation/student.validation.ts
+++ src/module/student/validation/student.validation.ts
@@ -1,5 +1,63 @@
-import * as z from "zod";
+import z from "zod";
+import { bloodGroups, GENDER_LIST } from "../interface/student.interface";
 
-const studentValidationSchema = z.object({ 
-    id: z.string().trim().min(1, "ID is required"),
-  });
+const studentValidationSchema = z.object({
+  id: z.string().min(1),
+  name: z.object({
+    firstName: z.string().min(1),
+    middleName: z.string().optional(),
+    lastName: z.string().min(1),
+  }),
+  email: z.string().min(1),
+  phone: z.string().min(1),
+  dateOfBirth: z.string().min(1),
+  address: z.string().min(1),
+  gender: z.enum(GENDER_LIST),
+  profileImage: z.string().optional(),
+  guardian: z
+    .object({
+      name: z.object({
+        firstName: z.string().min(1),
+        middleName: z.string().optional(),
+        lastName: z.string().min(1),
+      }),
+      email: z.string().min(1),
+      phone: z.string().min(1),
+      address: z.string().optional(),
+    })
+    .optional(),
+  parent: z
+    .array(
+      z.object({
+        id: z.string().min(1),
+        name: z.object({
+          firstName: z.string().min(1),
+          middleName: z.string().optional(),
+          lastName: z.string().min(1),
+        }),
+        email: z.string().min(1),
+        phone: z.string().min(1),
+        address: z.string().optional(),
+      })
+    )
+    .optional(),
+  localGuardian: z
+    .object({
+      name: z.object({
+        firstName: z.string().min(1),
+        middleName: z.string().optional(),
+        lastName: z.string().min(1),
+      }),
+      email: z.string().min(1),
+      phone: z.string().min(1),
+      address: z.string().optional(),
+      occupation: z.string().min(1),
+    })
+    .optional(),
+  bloodGroup: z.enum(bloodGroups).optional(),
+  presentAddress: z.string().min(1),
+  permanentAddress: z.string().min(1),
+  isActive: z.boolean(),
+  status: z.enum(["active", "inactive", "pending", "blocked", "deleted"]).optional(),
+  isDeleted: z.boolean(),
+});
