61f7cbcb90a76b7c45db908d3c4bd621d4ac7193
NF RIDOY <nfridoy@gmail.com>
2025-09-27 15:56:13 +0600

    ✨ feat(academicSemester): add year field to validation schema
    - Introduced a new 'year' field of type date in the academic semester validation schema to enhance the creation process of academic semesters.
    - This addition ensures that the year is explicitly validated alongside other semester attributes.

--- src/module/academicSemester/academicSemester.validation.ts
+++ src/module/academicSemester/academicSemester.validation.ts
@@ -5,6 +5,7 @@ export const createAcademicSemesterValidationSchema = z.object({
   body: z.object({
     name: z.enum(AcademicSemesterNames),
     code: z.enum(AcademicSemesterCodes),
+    year: z.date(),
     startMonth: z.enum(Months),
     endMonth: z.enum(Months), 
   })
