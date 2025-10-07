c484b6100c61d651216bcbf07541584268bebe5a
nfridoy <nfridoy@gmail.com>
2025-10-07 11:43:14 +0600

    🌱  TODO: feat(student.routes): add commented-out update and delete routes for future implementation
    - Introduced placeholder routes for updating and deleting students, preparing the structure for future enhancements in student management functionality.

--- src/module/student/student.route.ts
+++ src/module/student/student.route.ts
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post('/create-student', studentController.createStudent);
 router.get('/', studentController.getAllStudents);
 router.get('/:id', studentController.getSingleStudent);
+// router.patch('/:id', studentController.updateStudent);
+// router.delete('/:id', studentController.deleteStudent);
 
 export const studentRoute = router;
