64ae79485dca9d5f2b135fc2d3b8c0a22ff96718
nfridoy <nfridoy@gmail.com>
2025-10-07 11:41:58 +0600

    ✨ feat(user.service): implement transaction handling for student creation
    - Added MongoDB session management to ensure atomicity during user and student creation processes.
    - Enhanced error handling to rollback transactions in case of failures, improving data integrity.
    - Updated user creation logic to utilize session for better performance and reliability.

--- src/module/common/user/user.service.ts
+++ src/module/common/user/user.service.ts
@@ -7,6 +7,7 @@ import { IUser } from "./user.interface";
 import { UserModel } from "./user.model";
 import { generateStudentId } from "./user.utils";
 import AppError from "../../../errors/AppError";
+import mongoose from "mongoose";
 
 
 const createUserIntoDB = async (user: IUser): Promise<IUser> => {
@@ -14,46 +15,59 @@ const createUserIntoDB = async (user: IUser): Promise<IUser> => {
     return result;
 }
 const createStudentIntoDB = async (password: string, studentData: IStudent) => {
-    console.log({ password })
-    console.log({ studentData })
-    const userData: Partial<IUser> = {};
-
-    userData.password = password || config.default_pass as string;
-
-    //set student role
-    userData.role = "student";
-
-    // get academicSemester
-    const academicSemester = await AcademicSemesterModel.findOne({
-        _id: studentData?.admissionSemester
-    })
-
-    if (!academicSemester) {
-        throw new AppError(404, 'Academic semester not found');
-    }
-
-    //set manually generated it
-    // userData.id = '2030100002';
-    // userData.id = String('2025' + Math.floor(100000 + Math.random()* 900000));
+    const session = await mongoose.startSession();
+    try {
+        session.startTransaction();
+        console.log({ password })
+        console.log({ studentData })
+        const userData: Partial<IUser> = {};
     
-    // Genareted ID
-    userData.id = await generateStudentId(academicSemester);
+        userData.password = password || config.default_pass as string;
+    
+        //set student role
+        userData.role = "student";
+    
+        // get academicSemester
+        const academicSemester = await AcademicSemesterModel.findOne({
+            _id: studentData?.admissionSemester
+        })
+    
+        if (!academicSemester) {
+            throw new AppError(404, 'Academic semester not found');
+        }
+    
+        //set manually generated it
+        // userData.id = '2030100002';
+        // userData.id = String('2025' + Math.floor(100000 + Math.random()* 900000));
+        
+        // Genareted ID
+        userData.id = await generateStudentId(academicSemester);
+    
+        //set status
+        userData.status = "in-progress";
+    
+        const newUser = await UserModel.create([userData], {session})
+        console.log('newUser', newUser);
+        
+        if(!newUser.length) throw new AppError(500, "User Creation Failed")
+    
+        //create a student
+        // if (Object.keys(newUser).length) {
+        if (newUser.length && studentData) {
+            // set id as student id, _id as user
+            studentData.id = newUser[0].id;
+            studentData.user = newUser[0]._id; //reference _id
+    
+            const newStudent = await Student.create([studentData], {session});
 
-    //set status
-    userData.status = "in-progress";
-
-    const newUser = await UserModel.create(userData)
-    console.log('newUser', newUser);
-
-    //create a student
-    // if (Object.keys(newUser).length) {
-    if (newUser && studentData) {
-        // set id as student id, _id as user
-        studentData.id = newUser.id;
-        studentData.user = newUser._id; //reference _id
-
-        const newStudent = await Student.create(studentData);
-        return newStudent;
+            await session.commitTransaction()
+            await session.endSession()
+            return newStudent;
+        }
+    } catch (err) {
+        await session.abortTransaction()
+        await session.endSession();
+        throw new AppError(500, "Student Creation Into DB Faild", err);
     }
 }
 const getAllUserFromDB = async (): Promise<IUser[]> => {
