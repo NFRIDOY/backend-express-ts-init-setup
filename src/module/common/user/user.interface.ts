c146c29c1508a03b952177d987e4a8b5ee8aea74
nfridoy <nfridoy@gmail.com>
2025-10-13 12:51:48 +0600

    ♻️ refactor(user.interface): update user roles and status handling
    - Replaced hardcoded user roles with dynamic values from UserRole constant for better maintainability.
    - Updated status handling to utilize values from Status constant, enhancing consistency across the user interface.
    - Improved type definitions for roles and statuses to leverage TypeScript's capabilities.

--- src/module/common/user/user.interface.ts
+++ src/module/common/user/user.interface.ts
@@ -1,15 +1,22 @@
+import { Status, UserRole } from "./user.constant";
+
 export type IBloodGroup = "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
 export const bloodGroups: IBloodGroup[] = [ "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-",];
 
-export type IRole = 'admin' | 'student' | 'faculty';
-export const userRoles: IRole[]= ['admin' , 'student' , 'faculty'] as const;
+// old
+// export type IRole = 'admin' | 'student' | 'faculty';
+// export const userRoles: IRole[]= ['admin' , 'student' , 'faculty'] as const;
+// new
+export const userRoles = Object.values(UserRole); // array
+export type IRole = typeof UserRole[keyof typeof UserRole]; // Type
 
 // export type IStatus = 'active' | 'in-progress' | 'blocked';
 // export const statusList: IStatus[] = ['active', 'in-progress', 'blocked'] as const;
 
 // DRY Version
-export const statusList = ['in-progress', 'active', 'blocked'] as const;
-export type IStatus = typeof statusList[number];        // typeof array[number]
+// export const statusList = ['in-progress', 'active', 'blocked'] as const;
+export const statusList = Object.values(Status); // array
+export type IStatus = typeof statusList[number]; // typeof array[number]
 
 export const GENDER_LIST = ["male", "female", "other"] as const;
 export type IGender = "male" | "female" | "other";
