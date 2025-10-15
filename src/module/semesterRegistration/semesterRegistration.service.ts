ac03f063002d4bc7b4de3dd97c769209abc5df17
nfridoy <nfridoy@gmail.com>
2025-10-15 18:17:27 +0600

    ✨ feat(semesterRegistration): add validation for semester existence and registration checks
    - Implemented checks to verify the existence of the academic semester before registration.
    - Added validation to prevent duplicate semester registrations, enhancing data integrity and user feedback with appropriate error messages.

--- src/module/semesterRegistration/semesterRegistration.service.ts
+++ src/module/semesterRegistration/semesterRegistration.service.ts
@@ -1,22 +1,47 @@
 
 import QueryBuilder from "../../builder/QueryBuilder";
+import AppError from "../../errors/AppError";
+import { AcademicSemesterModel } from "../academicSemester/academicSemester.model";
 import { semesterRegistrationSearchableFields } from "./semesterRegistration.constant";
 import { ISemesterRegistration } from "./semesterRegistration.interface";
 import { SemesterRegistrationModel } from "./semesterRegistration.model";
 
 const createSemesterRegistrationIntoDB = async (payload: ISemesterRegistration) => {
+
+    // check if the semester is exist
+    const isAcademicSemesterExists =
+        await AcademicSemesterModel.findById(payload?.academicSemester);
+
+    if (!isAcademicSemesterExists) {
+        throw new AppError(
+            404,
+            'This academic semester not found !',
+        );
+    }
+
+    // check if the semester is already registered!
+    const isSemesterRegistrationExists = await SemesterRegistrationModel.findOne({
+        academicSemester: payload?.academicSemester,
+    });
+
+    if (isSemesterRegistrationExists) {
+        throw new AppError(
+            409,
+            'This semester is already registered!',
+        );
+    }
+
     const result = await SemesterRegistrationModel.create(payload)
     return result;
 }
 const getAllSemesterRegistrationFromDB = async (query: Record<string, unknown>) => {
 
     const SemesterRegistrationQuery = new QueryBuilder(SemesterRegistrationModel.find().populate('academicSemester'),
-    query)
-    .search(semesterRegistrationSearchableFields)
-    .filter()
-    .sort()
-    .paginate()
-    .fields()
+        query)
+        .filter()
+        .sort()
+        .paginate()
+        .fields()
     const result = await SemesterRegistrationQuery.modelQuery;
     return result;
 }
