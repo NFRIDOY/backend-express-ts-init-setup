530e83079ce40c009be2ef208d4ac9ebed185d86
nfridoy <nfridoy@gmail.com>
2025-10-21 17:49:40 +0600

    🔄 update(env): replace .env.rar file for configuration consistency

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,6 +10,7 @@ export default{
     database_url: process.env.DATABASE_URL,
     default_pass: process.env.DEFAULT_PASS,
     bcrypt_salt: process.env.BCRYPT_SALT,
+    jwt_secret_key: process.env.JWT_SECRET_KEY,
 }
 
 export const constants = {
