b0f9d77766b3e5e6459388ce0c5930c0e4b1bef8
nfridoy <nfridoy@gmail.com>
2025-11-03 11:14:05 +0600

    🔒 refactor(auth): update JWT payload type in auth_sturcture middleware
    - Changed the type of the decoded JWT payload from IjwtPayload to JwtPayload for improved type safety and clarity.
    - Minor cleanup in the auth middleware to enhance code readability.

--- src/middleware/auth_sturcture.ts
+++ src/middleware/auth_sturcture.ts
@@ -1,8 +1,7 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { UserRole } from '../module/common/user/user.constant';
-import { IjwtPayload } from '../module/auth/auth.interface';
 import AppError from '../errors/AppError';
 
 const auth: RequestHandler = (req, res, next) => {
@@ -10,7 +9,7 @@ const auth: RequestHandler = (req, res, next) => {
     console.log("authHeader", token)
 
     // verify a token symmetric - synchronous
-    const decoded = jwt.verify(token as string, config.jwt_access_secret as string) as IjwtPayload;
+    const decoded = jwt.verify(token as string, config.jwt_access_secret as string) as JwtPayload;
     console.log(decoded) 
     if(decoded?.userRole == UserRole.ADMIN) {
         next()
