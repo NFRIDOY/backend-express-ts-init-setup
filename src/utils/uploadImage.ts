e675b093c37c397a758846210097167ce37da229
nfridoy <nfridoy@gmail.com>
2025-11-16 15:42:38 +0600

    ✅ update(upload): refactor upload directory handling and enhance file naming
    - Simplified the logic for determining the upload directory for images.
    - Ensured the upload directory is created if it doesn't exist.
    - Improved file naming to include the original file extension along with a unique suffix during uploads.

--- src/utils/uploadImage.ts
+++ src/utils/uploadImage.ts
@@ -3,18 +3,28 @@ import path from "path";
 import fs from 'fs';
 
 // Ensure upload directory exists. and genarate the path if needed.
-const uploadDir = path.join(process.cwd(), 'uploads/images');
+let uploadDir = path.join(process.cwd(), 'uploads')
+
+// if(isImage) {
+//   uploadDir = path.join(process.cwd(), 'uploads/images');
+// }
+uploadDir = path.join(process.cwd(), 'uploads/images');
+
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, process.cwd() + '/uploads/images')
+    cb(null, uploadDir)
   },
   filename: function (req, file, cb) {
+    console.log("file", file)
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
+    const extension = file?.originalname.split('.').pop() || 'unknown';
+
+    cb(null, file.fieldname + '-' + uniqueSuffix + "." + extension)
   },
 
 })
